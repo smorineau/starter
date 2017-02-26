6eca408b25c4794c0989c6cb95963f4e37380e4a
Maxime Beugnet <maxime.beugnet@gmail.com>
2017-02-26 02:43:29 +0100

    added -i on sed.

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 if [[ ${1} =~ ^[a-z][0-9a-z-]*$ ]]; then
     rm -rf .git
-    sed s/starter/$1/g pom.xml
+    sed -i s/starter/$1/g pom.xml
     git init
     echo "git remote add origin <LINK>"
     echo "git push -u origin master"
     rm -f init.sh
 else
     echo "./init project-name"
-    echo "'project-name' has to validate regex ^[a-z][0-9a-z-]*$"
+    echo "'project-name' must validate the regex \"^[a-z][0-9a-z-]*$\"."
 fi
