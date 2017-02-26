f64e6638df8070f1140bfc266722679196f303ba
Maxime Beugnet <maxime.beugnet@gmail.com>
2017-02-26 02:40:07 +0100

    added init script

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+if [[ ${1} =~ ^[a-z][0-9a-z-]*$ ]]; then
+    rm -rf .git
+    sed s/starter/$1/g pom.xml
+    git init
+    echo "git remote add origin <LINK>"
+    echo "git push -u origin master"
+    rm -f init.sh
+else
+    echo "./init project-name"
+    echo "'project-name' has to validate regex ^[a-z][0-9a-z-]*$"
+fi
